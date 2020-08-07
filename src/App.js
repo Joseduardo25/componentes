746151664139e7f22ba426a81401de9bd4509357
Eduardo Calachua <jocdwardo25@gmail.com>
2020-08-07 16:00:00 -0500

    componente_avatar_3

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
 import React from 'react';
-
-
 const Calachua = (props) => {
   return(
     <div>
-      <img 
-        src={props.imagen} 
-        style={{
-          width:'50px', 
-          height:'50px',
-          borderRadius:'50%'
-        }}
-      />
-      <div>
-        {props.texto}
-      </div>
+      <img src={props.imagen} style={{width:'50px', heigth:'50px', borderRadius:'50%'}}/>
+  <div>{props.texto}</div>    
     </div>
   )
 }
 const avatares = [
   {
-    imagen:'https://static1.abc.es/media/cultura/2019/11/11/nueva-cancion-billie-eilish-k82B--620x349@abc.jpg',
-    texto:'Billie'
+    imagen:'https://upload.wikimedia.org/wikipedia/commons/d/d9/J_Balvin_2019.png',
+    texto:'J Balvin'
   },
   {
-    imagen:'https://dam.vanidades.com/wp-content/uploads/2019/05/Cardi-b-770x513.jpg',
-    texto:'Cardi B'
+    imagen:'https://mui.today/__export/1583369932727/sites/mui/img/2020/03/04/mezca-ozuna-foto-main-1234-1_x1x.jpg_423682103.jpg',
+    texto:'Ozuna'
   }
 ]
+
 function App() {
   return (
-   <div>
-     {avatares.map((e)=>
-     (<Calachua 
-     imagen={e.imagen} 
-     texto={e.texto}
-     />
-     ))}
-   </div>
+  <div>
+    {avatares.map((e)=>(<Calachua imagen={e.imagen} texto={e.texto}/>))}
+  </div>
    
     
   );
